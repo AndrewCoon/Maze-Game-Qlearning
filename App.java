716eb24a5ef33c8098931528be83099be2211ea3
Androo <84476053+Anduwu1@users.noreply.github.com>
2022-12-14 08:48:21 -0700

    Create App.java

---
+++ App.java
@@ -0,0 +1,10 @@
+public class App {
+  public static void main(String[] args) {
+    // create a new instance of the game
+    MazeGame game = new MazeGame();
+    
+    // initialize the game and start the main game loop
+    game.init();
+    game.start();
+  }
+}
